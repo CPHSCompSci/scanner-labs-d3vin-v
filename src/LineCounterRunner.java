64156ba8b4ccd39e61aaf864e2e011532aefd798
vasqudev <vasqudev@014-1617-S05NK.students.cisd.conroeisd.loc>
2019-10-22 11:58:58 -0500

    test

--- src/LineCounterRunner.java
+++ src/LineCounterRunner.java
@@ -10,8 +10,12 @@ public class LineCounterRunner {
 	public static void main(String args[]) {
 		Scanner file = new Scanner("LineCounter.txt");
 		
+		while(file.hasNextLine()) {
+			System.out.println(file.nextLine());
+		}
+		//LineCounter.LineCounter(file.nextLine());
+		//System.out.println();
 		
-		LineCounter.LineCounter(file.nextLine());
-		System.out.println();
+		file.close();
 	}
 }
